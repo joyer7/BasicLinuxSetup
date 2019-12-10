d79855efcc6dcd2a0845afc88dfa25ed11577b41
injoyer <injoyer@gmail.com>
2019-12-10 17:50:59 +0900

    First Commit

---
+++ setup-custom.sh
@@ -0,0 +1,111 @@
+#! /bin/bash
+echo "---------------------------------"
+echo "-- Start of Setup              --"
+echo "-- Author : Sam Rho            --"
+echo "-- Date : 2019/12/24           --"
+echo "---------------------------------"
+
+echo "-- Configure and optimize the OS"
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/rc.d/rc.local
+echo "echo never > /sys/kernel/mm/transparent_hugepage/defrag" >> /etc/rc.d/rc.local
+
+# add tuned optimization
+echo  "vm.swappiness = 1" >> /etc/sysctl.conf
+sysctl vm.swappiness=1
+timedatectl set-timezone Asia/Seoul
+
+# CDSW requires Centos 7.5
+# echo "CentOS Linux release 7.5.1810 (Core)" > /etc/redhat-release
+
+echo "-- Install Java OpenJDK8 and other tools"
+yum install -y java-1.8.0-openjdk-devel vim wget curl git bind-utils
+
+echo "-- Installing requirements for Stream Messaging Manager"
+yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash - 
+yum install nodejs -y
+npm install forever -g 
+
+# Check input parameters
+case "$1" in
+        aws)
+            echo "server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4" >> /etc/chrony.conf
+            systemctl restart chronyd
+            ;;
+        azure)
+            umount /mnt/resource
+            mount /dev/sdb1 /opt
+            ;;
+        gcp)
+            ;;
+        *)
+            echo $"Usage: $0 {aws|azure|gcp} template-file [docker-device]"
+            echo $"example: ./setup.sh azure default_template.json"
+            echo $"example: ./setup.sh aws cdsw_template.json /dev/xvdb"
+            exit 1
+esac
+
+
+echo "-- Configure networking"
+PUBLIC_IP=`curl https://api.ipify.org/`
+hostnamectl set-hostname `hostname -f`
+echo "`hostname -I` `hostname`" >> /etc/hosts
+sed -i "s/HOSTNAME=.*/HOSTNAME=`hostname`/" /etc/sysconfig/network
+systemctl disable firewalld
+systemctl stop firewalld
+setenforce 0
+sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+
+
+echo "-- Install CM and MariaDB repo"
+wget https://archive.cloudera.com/cm6/6.3.0/redhat7/yum/cloudera-manager.repo -P /etc/yum.repos.d/
+
+## MariaDB 10.1
+cat - >/etc/yum.repos.d/MariaDB.repo <<EOF
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+EOF
+
+yum clean all
+rm -rf /var/cache/yum/
+yum repolist
+
+yum install -y MariaDB-server MariaDB-client
+cat conf/mariadb.config > /etc/my.cnf
+
+
+echo "--Enable and start MariaDB"
+systemctl enable mariadb
+systemctl start mariadb
+
+echo "-- Install JDBC connector"
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz -P ~
+tar zxf ~/mysql-connector-java-5.1.46.tar.gz -C ~
+mkdir -p /usr/share/java/
+cp ~/mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar
+rm -rf ~/mysql-connector-java-5.1.46*
+
+echo "-- Enable passwordless root login via rsa key"
+ssh-keygen -f ~/myRSAkey -t rsa -N ""
+mkdir ~/.ssh
+cat ~/myRSAkey.pub >> ~/.ssh/authorized_keys
+chmod 400 ~/.ssh/authorized_keys
+ssh-keyscan -H `hostname` >> ~/.ssh/known_hosts
+#sed -i 's/.*PermitRootLogin.*/PermitRootLogin without-password/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+
+echo "-- Now CM is started and the next step is to automate using the CM API"
+yum install -y epel-release
+yum install -y python-pip
+pip install --upgrade pip
+pip install cm_client
+
+echo "-----------------------"
+echo "-- Complete of Setup --"
+echo "-----------------------"
